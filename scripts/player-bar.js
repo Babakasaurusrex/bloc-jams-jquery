4db2c991ac3077534898079ea2190b38f91a26fd
Babak Javdan <Babakjav.done@gmail.com>
2017-11-06 10:38:04 -0500

    Final commit for checkpoint

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,31 +1,15 @@
- {
+{
   $('button#play-pause').on('click', function() {
-    player.playPause();
-    $(this).attr('playState', player.playState);
-    $('#time-control .total-time').text( player.prettyTime(player.getDuration()) );
-  })
+    player.playPause(song);
+    $('button#play-pause').attr('playState', player.playState);
+  });
 
   $('button#next').on('click', function() {
-    if (player.playState !== 'playing') { return; }
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) { return; }
-
-    const nextSong = album.songs[nextSongIndex];
-    setSong(nextSong);
-
-  });
-
-  $('button#previous').on('click', function() {
-    if (player.playState !== 'playing') { return; }
-
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex < 0) { return; }
-
-    const previousSong = album.songs[previousSongIndex];
-    setSong(previousSong);
-
-  });
+     if (player.playState !== 'playing') { return; }
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const nextSongIndex = currentSongIndex + 1;
+     if (nextSongIndex >= album.songs.length) { return; }
+     const nextSong = album.songs[nextSongIndex];
+     player.playPause(nextSong);
+   });
 }
--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,25 +1,23 @@
-$( document ).ready( function() {
+{
   album.songs.forEach( (song, index) => {
     song.element = $(`
-      <tr>
-        <td>
-          <button>
-            <span class="song-number"> ${index + 1}</span>
-            <span class="ion-play"></span>
-            <span class="ion-pause"></span>
-          </button>
-        </td>
-        <td>${song.title}</td>
-        <td>${player.prettyTime(song.duration)}</td>
-      </tr>
-    `);
+       <tr>
+       <td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </button>
+         </td>
+       <td>${song.title}</td>
+       <td>${song.duration}</td>
+        </tr>
+     `);
 
-    song.element.click( event => {
-      player.playPause(song);
-      $('button#play-pause').attr('playState', player.playState);
-      $('#time-control .total-time').text( player.prettyTime(song.duration) );
-    });
+     song.element.on('click', event => {
+       player.playPause(song);
+     });
 
-    $( '#song-list' ).append(song.element);
+      $('#song-list').append(song.element);
   });
-});
+}
